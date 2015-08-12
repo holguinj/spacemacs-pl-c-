acae1e32a8dcead0847aa3953d950acc9ac71eb8
Justin Holguin <justin.holguin@puppetlabs.com>
2015-08-12 13:38:04 -0700

    First bit of semi-useful code

---
+++ README.md
@@ -0,0 +1,6 @@
+# (Un)official Puppet Labs C++ Style Layer for Spacemacs
+
+## Description
+
+This layer further configures the [Spacemacs](https://github.com/syl20bnr/spacemacs) C/C++ layer to change the indentation settings. Are they better this way? Who knows!
+
--- packages.el
+++ packages.el
@@ -1,10 +1,7 @@
 ;;; packages.el --- pl-c++ Layer packages File for Spacemacs
 ;;
-;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
-;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
+;; Author: Justin Holguin <justin.h.holguin@gmail.com>
+;; URL: https://github.com/holguinj/spacemacs-pl-cpp
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -13,12 +10,30 @@
 ;; List of all packages to install and/or initialize. Built-in packages
 ;; which require an initialization must be listed explicitly in the list.
 (setq pl-c++-packages
-    '(
-      ;; package names go here
-      ))
+    '(cc-mode))
 
 ;; List of packages to exclude.
 (setq pl-c++-excluded-packages '())
+
+(defun pl-c++--load-relative-file (file-path)
+  "Loads (as elisp source) the file at filepath relative to this layer's base directory."
+  (let* ((base (concat (configuration-layer/get-layer-path 'pl-c++)
+                       "pl-c++/"))
+         (full-path (concat base file-path)))
+    (load full-path)))
+
+(defun pl-c++/init-cc-mode ()
+  "Initialize pl-c++"
+  (use-package cc-mode
+    :defer t
+    :init
+    (progn
+      (pl-c++--load-relative-file "config.el")
+      (defun pl-c++-hook ()
+        (c-add-style "Google" google-c-style t)
+        (c-set-style "Google")
+        (c-toggle-hungry-state 1))
+      (add-hook 'c++-mode-hook 'pl-c++-hook))))
 
 ;; For each package, define a function pl-c++/init-<package-name>
 ;;
